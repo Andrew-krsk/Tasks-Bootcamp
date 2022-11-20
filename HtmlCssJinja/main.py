1d0e2309132ebfc12e45e5f76077a4ecf6410c1a
Andrew <>
2022-11-20 15:40:21 +0700

    Буткэмп Post, Get, Создание форм 20.11.22

--- HtmlCssJinja/main.py
+++ HtmlCssJinja/main.py
@@ -1,22 +1,33 @@
 from flask import Flask, render_template
+from LoginForm import LF
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hello hello hello hello hello world'
+
 
 
 @app.route('/')
 def main():
-    with open('File.txt', 'r', encoding = 'utf-8') as file:
-        resultData = list()
-        for line in file.readlines():
-            resultData.append(tuple(line.split('\n')[0].split(';')))
-
-    print(resultData)
-    return render_template('base.html', data=resultData)
+   return render_template('base.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def reg():
+    form = LF()
+    if form.validate_on_submit():
+        if form.password_again.data != form.password.data:
+            return render_template('register.html', tittle="Регистрация", form=form,
+                                    message = "Пароли не совпадают!!!")
+
+        with open('file.txt', 'a', encoding='utf-8') as file:
+            file.write(f'{form.name.data};{form.email.data};{form.password.data}')
+        return render_template('register.html', message = "Регистрация прошла успешно")
+    return render_template('register.html', tittle="Регистрация", form=form)
+
 
 
 if __name__ == '__main__':
